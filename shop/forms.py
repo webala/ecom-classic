10678e4ce6324ed4eed93c0597f714927364e7ba
webala <webdspam@gmail.com>
2022-08-03 16:51:27 +0300

    product and category forms added

--- shop/forms.py
+++ shop/forms.py
@@ -1,6 +1,14 @@
+from dataclasses import field
 from django import forms
+from .models import Category, Product
 
 class ProductForm (forms.ModelForm):
     class Meta:
         model = Product
-        
+        fields = ['name', 'price', 'category', 'inventory', 'image']
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        field = ['name']
