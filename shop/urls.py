618c7a8e293eefea4698a0779d5efca5d0f168cb
webala <webdspam@gmail.com>
2022-08-03 19:05:26 +0300

    category create view added

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard
+from .views import CategoryCreate, dashboard
 
 urlpatterns = [
-    path('dashboard', dashboard, name='dashboard')
+    path('dashboard', dashboard, name='dashboard'),
+    path('category/add', CategoryCreate.as_view(), name='category_create')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+
+from shop.models import Category
 
 # Create your views here.
 
 
 def dashboard(request):
 
-    
+
     context = {}
 
     return render(request, 'dashboard.html', context)
+
+class CategoryCreate(CreateView):
+    model = Category
+    fields = ['name']
+    template_name = 'category_form.html'
