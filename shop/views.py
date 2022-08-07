3a28d7ff6dc435689cc77813dd214d68bf8f93e3
webala <webdspam@gmail.com>
2022-08-07 20:23:52 +0300

    navigation addded

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from unicodedata import category
 from django.shortcuts import render, get_object_or_404
+
 # from django.urls import reverse
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
@@ -11,33 +12,35 @@ from shop.models import Category, Product
 
 def dashboard(request):
 
-
     context = {}
 
-    return render(request, 'dashboard.html', context)
+    return render(request, "dashboard.html", context)
+
 
 class CategoryCreate(CreateView):
     model = Category
-    fields = ['name']
-    template_name = 'category_form.html'
-    success_url = '/shop/category/add'
+    fields = ["name"]
+    template_name = "category_form.html"
+    success_url = "/shop/category/add"
 
 
 class ProductCreate(CreateView):
     model = Product
-    fields = ['category', 'name', 'price', 'inventory', 'image']
-    template_name: str = 'product_form.html'
-    success_url = '/shop/product/add'
+    fields = ["category", "name", "price", "inventory", "image"]
+    template_name: str = "product_form.html"
+    success_url = "/shop/product/add"
+
 
 class ProductListView(ListView):
     model = Product
-    template_name: str = 'shop.html'
+    template_name: str = "shop.html"
     # context_object_name: str = 'products'
 
+
 class CategoryListView(ListView):
-    template_name: str = 'product_per_category.html'
-    
+    template_name: str = "product_per_category.html"
+
     def get_queryset(self):
 
-        self.category = get_object_or_404(Category, name=self.kwargs['category_name'])
+        self.category = get_object_or_404(Category, name=self.kwargs["category_name"])
         return Product.objects.filter(category=self.category)
