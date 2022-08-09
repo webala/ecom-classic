594fccde37a3b63bdaf699d867807934f8d635f5
webala <webdspam@gmail.com>
2022-08-09 07:45:55 +0300

    models for cart functionality added

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,10 @@
+
 from django.db import models
 
 # Create your models here.
 
+
+#Products will be grouped into different categories.
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
@@ -27,3 +30,57 @@ class Product(models.Model):
 # class Rating(models.Model):
 #     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 #     rating = models.IntegerChoices()
+
+
+#customer Details
+class Customer(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+
+#Delivery Address for products
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    location = models.CharField(max_length=20)
+    estate = models.CharField(max_length=20)
+    house_no = models.CharField(max_length=20)
+
+#This model will translate to cart in the client side
+class Order(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    processed = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.SET_NULL, null=True)
+
+    #this function returns the number of items in an order(cart)
+    @property
+    def cart_items(self):
+        cart_items = self.cartitem_set.all()
+        return sum([item.quantity for item in cart_items])
+    
+    #This function return the total price of the order
+    @property
+    def cart_total(self):
+        cart_items = self.cartitem_set.all()
+        return sum([item.item_total for item in cart_items])
+
+
+#individual items in an order(cart)
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+    @property
+    def item_total(self):
+        return self.quantity * self.product.price
+
+#Transaction details for paymetents made
+class TransactionDetails(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    transaction_code = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
+    
+
+
