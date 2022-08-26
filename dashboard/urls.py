144ab476c627facc835457ff6fe6de84331ea490
webala <webdspam@gmail.com>
2022-08-26 10:45:02 +0300

    ADD: customer list template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('products', dash_products, name='dash-products'),
     path('products/delete/<int:product_id>', delete_product, name='delete_product'),
-    path('product/edit/<pk>', UpdateProduct.as_view(), name='update-product')
+    path('product/edit/<pk>', UpdateProduct.as_view(), name='update-product'),
+    path('customers', CustomersView.as_view(), name='customers')
 ]
