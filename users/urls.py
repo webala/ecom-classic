f0c8a3f1737a8b683c1cf22495f11c35537495f6
webala <webdspam@gmail.com>
2022-10-11 08:05:03 +0300

    ADD: login and register

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from users.views import register_user
+
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register', register_user, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def register_user(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+    
+    return render(request, 'registration/register.html', context={'form': form})
